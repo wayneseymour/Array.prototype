2490d5159e345a8ecbf90a1292819861ebdff061
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-25 07:34:22 -0500

    Added more tests. Need more tests to flush out the truth about shallow copying using `[].slice()`.

--- __tests__/lazy-slice-test.js
+++ __tests__/lazy-slice-test.js
@@ -1,12 +1,30 @@
-import lazySlice from '../lazy-slice'
+import lazySlice from '../lazy-slice';
 
-describe('Lazy slice of ["zero", "one", "two", "three"]', () => {
-  const slice4 = lazySlice(['zero', 'one', 'two', 'three'])
-  
-  test('passed the params (1, 3)', () => {
-    expect(slice4(1, 3)).toEqual(["one", "two"])
+describe('Lazy slice', () => {
+  test('should be a function', () => {
+    expect(typeof lazySlice).toBe('function');
   });
-  test('passed the param (0, 1)', () => {
-    expect(slice4(0, 1)).toEqual(["zero"])
+  test('should return a function when invoked', () => {
+    expect(typeof lazySlice()).toEqual('function');
+  });
+  describe('of ["zero", "one", "two", "three"]', () => {
+    const arr4 = ['zero', 'one', 'two', 'three'];
+    const slice4 = lazySlice(arr4);
+
+    test('passed the params (1, 3) should return the second and third items', () => {
+      expect(slice4(1, 3)).toEqual(['one', 'two']);
+    });
+    test('passed the param (0, 1) should return only the first (zeroth) item', () => {
+      expect(slice4(0, 1)).toEqual(['zero']);
+    });
+    test('passed the params (1, 1) should return an empty array', () => {
+      expect(slice4(1, 1)).toEqual([]);
+    });
+
+    describe('in shallow copying', () => {
+      test('passed no params should return a shallow copy of the array', () => {
+        expect(slice4()).toEqual(['zero', 'one', 'two', 'three']);
+      });
+    });
   });
 });
