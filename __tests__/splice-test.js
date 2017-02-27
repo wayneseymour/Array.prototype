040b55a34fdc16884a17cbda1eb43f175a7f98ea
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-27 15:34:06 -0500

    Still working on 'er

---
+++ __tests__/splice-test.js
@@ -0,0 +1,89 @@
+// The splice() method changes the content of an array by
+// removing existing elements and/or adding new elements.
+
+// array.splice(start)
+// array.splice(start, deleteCount)
+// array.splice(start, deleteCount, item1, item2, ...)
+
+describe('Array.prototype.splice', () => {
+  describe('should be a function', () => {
+    test('should be a function in longhand', () => {
+      expect(typeof Array.prototype.splice).toBe('function');
+    });
+    test('should be a function in shorthand', () => {
+      expect(typeof [].splice).toEqual('function');
+    });
+  });
+  describe('Remove 0 elements from index 2, and insert "drum"', () => {
+    let myFish;
+    let removed;
+    beforeAll(() => {
+      myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+      removed = myFish.splice(2, 0, 'drum');
+    });
+    test('should not remove any indices', () => {
+      expect(removed).toEqual([]);
+    });
+    test('should add a new indice to the array', () => {
+      expect(myFish[2]).toBe('drum');
+    });
+  });
+  describe('Remove 1 element from index 3', () => {
+    let myFish;
+    let removed;
+    beforeAll(() => {
+      myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+      removed = myFish.splice(3, 1);
+    });
+    test('should have removed "sturgeon"', () => {
+      expect(removed).toEqual(['sturgeon']);
+    });
+    test('should have shortened the array', () => {
+      expect(myFish.length).toBe(3);
+    });
+  });
+
+  describe('Remove 1 element from index 2, and insert trumpet', () => {
+    let myFish;
+    let removed;
+    let length;
+    beforeAll(() => {
+      myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+      length = myFish.length;
+      removed = myFish.splice(2, 1, 'trumpet');
+    });
+    test('should have set index two to "trumpet"', () => {
+      expect(myFish[2]).toEqual('trumpet');
+    });
+    test('should not remove any indices', () => {
+      expect(removed).toEqual(['mandarin']);
+    });
+    test('should still have the same length', () => {
+      expect(length).toEqual(myFish.length);
+    });
+  });
+  describe(
+    `Remove 2 elements from index 0,
+    and insert "parrot", "anemone" and "blue"`,
+    () => {
+      let myFish;
+      let removed;
+      let length;
+      beforeAll(() => {
+        myFish = ['angel', 'clown', 'trumpet', 'sturgeon'];
+        length = myFish.length;
+        removed = myFish.splice(0, 2, 'parrot', 'anemone', 'blue');
+      });
+      test('should have set index two to "blue"', () => {
+        console.log(myFish);
+        expect(myFish[2]).toEqual('blue');
+      });
+      test('should not remove any indices', () => {
+        expect(removed).toEqual(['angel', 'clown']);
+      });
+      test('should have increased the length to five', () => {
+        expect(myFish.length).toEqual(5);
+      });
+    }
+  );
+});
