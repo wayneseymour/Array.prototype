82e6320aa11a57de86342ddba4a599ebc56e77e2
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-27 11:03:32 -0500

    Added src and test for Array.prototype.splice().

--- __tests__/lazy-splice-test.js
+++ __tests__/lazy-splice-test.js
@@ -1,3 +1,5 @@
+// The splice() method changes the content of an array by
+// removing existing elements and/or adding new elements.
 import lazySplice from '../lazy-splice';
 
 const eq = a => b => a === b;
@@ -9,28 +11,23 @@ describe('Lazy splice', () => {
   test('should return a function when invoked', () => {
     expect(typeof lazySplice()).toEqual('function');
   });
-  xdescribe('of ["zero", "one", "two", "three"]', () => {
-    const arr4 = ['zero', 'one', 'two', 'three'];
-    const slice4 = lazySplice(arr4);
-
-    test('passed the params (1, 3) should return the second and third items', () => {
-      expect(slice4(1, 3)).toEqual(['one', 'two']);
-    });
-    test('passed the param (0, 1) should return only the first (zeroth) item', () => {
-      expect(slice4(0, 1)).toEqual(['zero']);
-    });
-    test('passed the params (1, 1) should return an empty array', () => {
-      expect(slice4(1, 1)).toEqual([]);
-    });
-
-    describe('in shallow copying', () => {
-      const result = slice4();
-
-      test('should return false when a "sliced" array is compared to the array it was "sliced" from', () => {
-        expect(eq(arr4, result)()).toBe(false);
+  describe('of ["zero", "one", "two", "three"]', () => {
+    describe('mutation by removal of elements', () => {
+      let orig;
+      let mutated;
+      let arr4;
+      beforeAll(() => {
+        orig = ['zero', 'one', 'two', 'three'];
+        arr4 = lazySplice(orig);
+        mutated = arr4(1, 3);
       });
-      test('passed no params should return a shallow copy of the array', () => {
-        expect(result).toEqual(['zero', 'one', 'two', 'three']);
+      describe('passed the params (1, 3)', () => {
+        test('should return the second and third items', () => {
+          expect(mutated).toEqual(['one', 'two', 'three']);
+        });
+        test('should mutate the original array', () => {
+          expect(orig).toEqual(['zero']);
+        });
       });
     });
   });
--- lazy-splice.js
+++ lazy-splice.js
@@ -1,1 +1,3 @@
+// The splice() method changes the content of an array by
+// removing existing elements and/or adding new elements.
 export default xs => (a, b) => xs.splice(a, b);
