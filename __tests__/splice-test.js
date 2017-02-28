5e063884a3250a62fee791d16c2cdbd987046df6
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-28 10:09:37 -0500

    More tests

--- __tests__/splice-test.js
+++ __tests__/splice-test.js
@@ -6,7 +6,7 @@
 // array.splice(start, deleteCount, item1, item2, ...)
 
 describe('Array.prototype.splice', () => {
-  describe('should be a function', () => {
+  describe('type', () => {
     test('should be a function in longhand', () => {
       expect(typeof Array.prototype.splice).toBe('function');
     });
@@ -42,7 +42,20 @@ describe('Array.prototype.splice', () => {
       expect(myFish.length).toBe(3);
     });
   });
-
+  describe('Remove 1 element the end of an Array', () => {
+    let xs;
+    let removed;
+    beforeAll(() => {
+      xs = [1, 2, 3];
+      removed = xs.splice(xs.length - 1, 1);
+    });
+    test('should have removed the last item', () => {
+      expect(removed).toEqual([3]);
+    });
+    test('should have shortened the array', () => {
+      expect(xs).toEqual([1, 2]);
+    });
+  });
   describe('Remove 1 element from index 2, and insert trumpet', () => {
     let myFish;
     let removed;
@@ -75,7 +88,6 @@ describe('Array.prototype.splice', () => {
         removed = myFish.splice(0, 2, 'parrot', 'anemone', 'blue');
       });
       test('should have set index two to "blue"', () => {
-        console.log(myFish);
         expect(myFish[2]).toEqual('blue');
       });
       test('should not remove any indices', () => {
